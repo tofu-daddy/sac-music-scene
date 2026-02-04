1cd8e599a4fb3e9b44bd4cb7eebc221002067193
Toni Romero <t.romero.dev@gmail.com>
2026-02-03 17:08:47 -0800

    updated config file

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    plugins: [
-        tailwindcss(),
-    ],
+  base: "/pokadex/",
+  plugins: [tailwindcss()],
 })
